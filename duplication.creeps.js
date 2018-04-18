75a2a6a7241354d61887905f6fd49ee6c7acdd63
t-bird <38475344+t-bird79@users.noreply.github.com>
2018-04-18 13:35:30 +0200

    Update duplication.creeps.js

--- duplication.creeps.js
+++ duplication.creeps.js
@@ -1,6 +1,23 @@
 module.exports = {
   // Automatische Reproduktion der Creeps
   run () {
-    // console.log("test");
+    // Anzahl Creeps ermitteln
+    var creepcount = 0;
+    for (var sKey in Game.creeps) {
+      if (Game.creeps.hasOwnProperty(sKey)) creepcount++;
+    }
+    // Alle Spawns durchgehen
+    for(const i in Game.spawns) {
+      // Prüfen ob es schon Creeps gibt und falls nötig ersten Creep erstellen
+      if (!creepcount) {
+        // keine Creeps vorhanden, erstelle Harvester
+        console.log("keine Creeps vorhanden");
+        Game.spawns[i].spawnCreep([WORK, CARRY, MOVE, MOVE], "FIRST", { memory: {role: 'harvester'} });
+      }
+      else {
+        // Als nächstes .... ???
+        // console.log("Anzahl Creeps:"+creepcount);
+      }  
+    }
   }
 };
