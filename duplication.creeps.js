3e7629018e6b08949e0e23a31a0903126826a7bb
t-bird <38475344+t-bird79@users.noreply.github.com>
2018-04-19 13:06:52 +0200

    Update duplication.creeps.js

--- duplication.creeps.js
+++ duplication.creeps.js
@@ -8,14 +8,19 @@ module.exports = {
     }
     // Alle Spawns durchgehen
     for(const i in Game.spawns) {
-      // Prüfen ob es schon Creeps gibt und falls nötig ersten Creep erstellen
-      if (!creepcount) {
+      // Wie viel Energie steht dem Spawn zur Verfügung?  
+      var spenergy = Game.spawns[i].energy;
+      // Kosten des ersten Harvesters
+      var hprice = BODYPART_COST['work']+BODYPART_COST['carry']+2*BODYPART_COST['move'];
+      // console.log(hprice);
+      // PrÃ¼fen ob es schon Creeps gibt und falls nÃ¶tig ersten Creep erstellen
+      if (!creepcount & spenergy>=hprice) {
         // keine Creeps vorhanden, erstelle Harvester
         console.log("keine Creeps vorhanden");
         Game.spawns[i].spawnCreep([WORK, CARRY, MOVE, MOVE], "FIRST", { memory: {role: 'harvester'} });
       }
       else {
-        // Als nächstes .... ???
+        // Als nÃ¤chstes .... ???
         // console.log("Anzahl Creeps:"+creepcount);
       }  
     }
